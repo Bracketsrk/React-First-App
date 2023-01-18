e366d31f11a3547875d5efd564ac990100b88411
Elliot <liniguy@Oracle.local>
2023-01-17 22:49:53 -0800

    Added delete function

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -17,10 +17,13 @@ function TableHeader()  {
 function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
       return (
-        <tr key={index}>
-          <td>{row.name}</td>         
-          <td>{row.job}</td>
-        </tr>
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
       );
      }
     );
@@ -31,15 +34,18 @@ function TableBody(props) {
      );
 }
   
-    
-function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
-    );
-} 
+
+
+function Table (props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </table>
+  );
+}
+
+
 
 
 export default Table;
